908eda955555ba3863fdf769d349da1b72225cf0
Juan <92172817+JuanVillaverdeRodriguez@users.noreply.github.com>
2025-02-22 05:14:22 +0100

    arreglado fallo...

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Path
+from fastapi import FastAPI, File, HTTPException, UploadFile, Path
 import os
 
 app = FastAPI()
@@ -8,15 +8,21 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...)):
-    if not file.filename.lower().endswith((".jpg", ".jpeg")):
-        return {"error": "Solo se permiten archivos JPG, tu has subido:" + file.filename}
+    try:
+        # Verifica que el archivo es una imagen JPG o JPEG
+        if not file.filename.lower().endswith((".jpg", ".jpeg")):
+            raise HTTPException(status_code=400, detail="Solo se permiten archivos JPG")
 
-    file_location = Path(UPLOAD_FOLDER) / file.filename
+        file_location = Path(UPLOAD_FOLDER) / file.filename
 
-    with open(file_location, "wb") as f:
-        f.write(await file.read())
+        # Guarda la imagen en el servidor
+        with open(file_location, "wb") as f:
+            f.write(await file.read())
 
-    return {"url": f"https://lookfinderserver-production.up.railway.app/{file_location}"}
+        return {"url": f"https://lookfinderserver-production.up.railway.app/uploads/{file.filename}"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
 
 @app.get("/") 
 def read_root():
