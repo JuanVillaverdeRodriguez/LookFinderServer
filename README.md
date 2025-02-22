b487318e64432a6d0b430c70e6789090c74732f1
Juan <92172817+JuanVillaverdeRodriguez@users.noreply.github.com>
2025-02-22 04:34:04 +0100

    api

---
+++ README.md
@@ -0,0 +1,8 @@
+Hacer un venv:
+python -m venv .
+
+Iniciarlo:
+Windows: .\Scripts\Activate.ps1 
+
+Instalar fastapi:
+pip install fastapi
---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, File, UploadFile
+import os
+
+app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile = File(...)):
+    file_location = f"{UPLOAD_FOLDER}/{file.filename}"
+    
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+
+    return {"url": f"https://tu-servidor.com/{file_location}"}
